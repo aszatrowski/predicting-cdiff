7844d43b98f20a3ba58b603a225d338e65bcc99c
Austin Szatrowski <austinszatrowski@gmail.com>
2025-05-14 07:20:27 -0500

    update .r with liver, renal, bloods, global var, new summary functions

--- create_training_data.r
+++ create_training_data.r
@@ -1,7 +1,12 @@
 execution_start <- now()
+# Data loading and cleaning
+## Major settings:
+
 library(tidyverse)
-library(glue)
+time_delta <- ddays(30) # days
+
 antibiotics_list <- read_csv("hospital-antibiotics.csv")
+
 
 ## Generate sample of unique antibiotic administrations
 
@@ -19,18 +24,14 @@ antibiotics <- read_csv("mimic-demo/PRESCRIPTIONS.csv") |>
         antibiotics_list,
         by = c("drug" = "Antibiotic")
     ) |>
-    select(
-        row_id,
-        subject_id,
-        hadm_id,
-        icustay_id,
-        antibiotic_key,
-        drug,
-        drug_class,
-        ab_startdate,
-        ab_enddate,
-        dose_val_rx,
-        dose_unit_rx)
+    pivot_wider(
+        id_cols = c(subject_id, hadm_id, icustay_id, antibiotic_key, ab_startdate, ab_enddate),
+        names_from = drug_class,
+        values_from = drug_class,
+        values_fn = ~ 1,  # Set to 1 when present
+        values_fill = 0,   # Set to 0 when absent
+        names_prefix = "ab_class_is_"
+    )
 
 
 ## Load *C. diff* diagnosis info
@@ -52,7 +53,6 @@ cdiff_icd9 <- read_csv("mimic-demo/DIAGNOSES_ICD.csv") |>
     select(
         subject_id,
         hadm_id,
-        seq_num,
         cdiff_icd9
     )
 
@@ -62,13 +62,8 @@ diarrhea_icd9 <- read_csv("mimic-demo/DIAGNOSES_ICD.csv") |>
     select(
         subject_id,
         hadm_id,
-        seq_num,
         diarrhea_icd9
     )
-
-head(cdiff_tests)
-head(cdiff_icd9)
-head(diarrhea_icd9)
 
 
 ## Load patient demographics
@@ -95,7 +90,16 @@ demographics <- admissions |>
         admission_type,
         insurance,
         diagnosis,
+    ) |>
+    pivot_wider(
+        id_cols = c(subject_id:dischtime),
+        names_from = c(admission_type),
+        values_from = c(admission_type),
+        names_prefix = "admit_is_",
+        values_fn = ~ 1,
+        values_fill = 0
     )
+demographics
 
 
 ## Load ICU status
@@ -106,8 +110,6 @@ icustays <- read_csv("mimic-demo/ICUSTAYS.csv") |>
         subject_id,
         hadm_id,
         icustay_id,
-        first_careunit,
-        last_careunit,
         icu_intime,
         icu_outtime
     )
@@ -158,24 +160,145 @@ liver_labs <- labvalues |>
     rename(lab_charttime = charttime) |>
     # filter to labs *prior* to admin time
     filter(lab_charttime < ab_startdate) |>
+    # Summarize each lab value, grouped by administration instance
+    group_by(subject_id, hadm_id, antibiotic_key, test_name) |>
+    summarize(
+        mean = mean(valuenum, na.rm = TRUE),
+        median = median(valuenum, na.rm = TRUE),
+        max = max(valuenum, na.rm = TRUE),
+        min = min(valuenum, na.rm = TRUE),
+        # q10 = quantile(valuenum, 0.1, na.rm = TRUE),
+        # q90 = quantile(valuenum, 0.9, na.rm = TRUE),
+        range = max(valuenum, na.rm = TRUE) - min(valuenum, na.rm = TRUE),
+        sd = replace_na(sd(valuenum, na.rm = TRUE), 0),
+        iqr = IQR(valuenum, na.rm = TRUE),
+        first = first(valuenum, na_rm = TRUE),
+        last = last(valuenum, na_rm = TRUE),
+        count = length(valuenum),
+        .groups = "drop"
+    ) |>
+    # WARNING: NAs appear for patients with zero instances of a given lab; these are only implicit in the unpivoted data, since test_name will just skip that lab for that patien
     pivot_wider(
-        id_cols = c(
-            subject_id,
-            hadm_id,
-            antibiotic_key),
+        id_cols = c(subject_id, hadm_id, antibiotic_key),
         names_from = test_name,
-        values_from = c(valuenum),
-        # Average over each lab value type
-        values_fn = ~ mean(.x, na.rm = TRUE),
-        names_prefix = "mean_prior_")
+        values_from = !(c(subject_id, hadm_id, antibiotic_key, test_name)),
+        names_sep = "_"
+    )
+
+### Renal
+# NOTE: I CHOSE THE MOST COMMONLY APPEARING LAB EVENT VALUES AND SPECIFICALLY CHOSE FOR VALUES INSIDE THE BLOOD!
+
+renal_lab_itemids <- list(
+    creatinine = 50912,
+    bun = 51006,
+    potassium = 50971,
+    sodium = 50983,
+    chloride = 50902
+)
+
+# Set QC thresholds for lab values
+renal_qc_thresholds <- c(
+    "creatinine" = 15,
+    "bun" = 200,
+    "potassium" = 10,
+    "sodium" = 200,
+    "chloride" = 160
+)
+
+renal_labs <- labvalues |>
+    filter(itemid %in% renal_lab_itemids) |>
+    mutate(
+        test_name = as.factor(case_when(
+            itemid == renal_lab_itemids$creatinine ~ names(renal_lab_itemids)[1],
+            itemid == renal_lab_itemids$bun ~ names(renal_lab_itemids)[2],
+            itemid == renal_lab_itemids$potassium ~ names(renal_lab_itemids)[3],
+            itemid == renal_lab_itemids$sodium ~ names(renal_lab_itemids)[4],
+            itemid == renal_lab_itemids$chloride ~ names(renal_lab_itemids)[5]
+        ))
+    ) |>
+    filter(valuenum < renal_qc_thresholds[test_name]) |>
+    right_join(antibiotics, by = c("subject_id", "hadm_id"), relationship = "many-to-many") |>
+    rename(lab_charttime = charttime) |>
+    filter(lab_charttime < ab_startdate) |>
+    group_by(subject_id, hadm_id, antibiotic_key, test_name) |>
+    summarize(
+        mean = mean(valuenum, na.rm = TRUE),
+        median = median(valuenum, na.rm = TRUE),
+        max = max(valuenum, na.rm = TRUE),
+        min = min(valuenum, na.rm = TRUE),
+        range = max(valuenum, na.rm = TRUE) - min(valuenum, na.rm = TRUE),
+        sd = replace_na(sd(valuenum, na.rm = TRUE), 0),
+        iqr = IQR(valuenum, na.rm = TRUE),
+        first = first(valuenum, na_rm = TRUE),
+        last = last(valuenum, na_rm = TRUE),
+        count = length(valuenum),
+        .groups = "drop"
+    ) |>
+    pivot_wider(
+        id_cols = c(subject_id, hadm_id, antibiotic_key),
+        names_from = test_name,
+        values_from = !(c(subject_id, hadm_id, antibiotic_key, test_name)),
+        names_sep = "_"
+    )
 
 
-# Renal
-# Blood counts
+### Blood counts
 
-# Join everything together and compute new columns
+# item_id dictionary
+blood_lab_itemids <- list(
+    hemoglobin = 51222,
+    hematocrit = 51221,
+    white_blood_cells = 51300,
+    platelets = 51265
+)
+
+# Set QC thresholds for lab values
+blood_qc_thresholds <- c(
+    "hemoglobin" = 25,
+    "hematocrit" = 75,
+    "white_blood_cells" = 100,
+    "platelets" = 1500
+)
+
+blood_labs <- labvalues |>
+    filter(itemid %in% blood_lab_itemids) |>
+    mutate(
+        test_name = as.factor(case_when(
+            itemid == blood_lab_itemids$hemoglobin ~ names(blood_lab_itemids)[1],
+            itemid == blood_lab_itemids$hematocrit ~ names(blood_lab_itemids)[2],
+            itemid == blood_lab_itemids$white_blood_cells ~ names(blood_lab_itemids)[3],
+            itemid == blood_lab_itemids$platelets ~ names(blood_lab_itemids)[4]
+        ))
+    ) |>
+    filter(valuenum < blood_qc_thresholds[test_name]) |>
+    right_join(antibiotics, by = c("subject_id", "hadm_id"), relationship = "many-to-many") |>
+    rename(lab_charttime = charttime) |>
+    filter(lab_charttime < ab_startdate) |>
+    group_by(subject_id, hadm_id, antibiotic_key, test_name) |>
+    summarize(
+        mean = mean(valuenum, na.rm = TRUE),
+        median = median(valuenum, na.rm = TRUE),
+        max = max(valuenum, na.rm = TRUE),
+        min = min(valuenum, na.rm = TRUE),
+        range = max(valuenum, na.rm = TRUE) - min(valuenum, na.rm = TRUE),
+        sd = replace_na(sd(valuenum, na.rm = TRUE), 0),
+        iqr = IQR(valuenum, na.rm = TRUE),
+        first = first(valuenum, na_rm = TRUE),
+        last = last(valuenum, na_rm = TRUE),
+        count = length(valuenum),
+        .groups = "drop"
+    ) |>
+    pivot_wider(
+        id_cols = c(subject_id, hadm_id, antibiotic_key),
+        names_from = test_name,
+        values_from = !(c(subject_id, hadm_id, antibiotic_key, test_name)),
+        names_sep = "_"
+    )
+
+
+## Join everything together and compute new columns
+
 training_data <- antibiotics |>
-    select(-row_id) |>
     left_join(
         cdiff_tests,
         by = c("subject_id", "hadm_id")
@@ -200,6 +323,14 @@ training_data <- antibiotics |>
         liver_labs,
         by = c("subject_id", "hadm_id", "antibiotic_key"),
     ) |>
+    left_join(
+        renal_labs,
+        by = c("subject_id", "hadm_id", "antibiotic_key"),
+    ) |>
+    left_join(
+        blood_labs,
+        by = c("subject_id", "hadm_id", "antibiotic_key"),
+    ) |>
     mutate(
         # generate age column
         age_at_admin = ab_startdate - dob,
@@ -210,12 +341,13 @@ training_data <- antibiotics |>
             TRUE,
             FALSE
         ),
+        admission_time_of_day = hour(admittime),
         # GENERATE LABEL:
         cdiff_30d_flag = ifelse(
             # OPTION 1: C. diff positive culture after Ab start and less than 30 days after
-            (cdiff_charttime >= ab_startdate & cdiff_charttime <= ab_startdate + ddays(30))
+            (cdiff_charttime >= ab_startdate & cdiff_charttime <= ab_startdate + time_delta)
             # OPTION 2: C. diff ICD (00845) assigned at end of stay, as long as discharge was <30 days after Ab start
-            | (cdiff_icd9 == "00845" & dischtime <= ab_startdate + ddays(30)),
+            | (cdiff_icd9 == "00845" & dischtime <= ab_startdate + time_delta),
             # Assign TRUE/FALSE accordingly
             yes = TRUE,
             no = FALSE
@@ -223,7 +355,8 @@ training_data <- antibiotics |>
         # ifelse() returns NA for missing data (non-Cdiff patients will have missing ICDs), so fill missing with FALSE
         cdiff_30d_flag = replace_na(cdiff_30d_flag, FALSE)
     )  |>
-    select(-icu_intime, -icu_outtime)
+    select(-icu_intime, -icu_outtime, -dischtime)
+# View(training_data)
 
 process_stop <- now()
 write_csv(training_data, "training_data.csv", progress = TRUE)
