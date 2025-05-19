1549815bc3729fa68e1ba201b7868e55f3dbd9d6
Austin Lucas Ted Szatrowski <aszatrowski@midway3-login3.rcc.local>
2025-05-19 16:12:27 -0500

    imputation, train-test, initial training run

--- impute_missing_data_fullset.py
+++ impute_missing_data_fullset.py
@@ -1,27 +1,46 @@
 # %% [markdown]
 # # Missing Data Imputation for `predicting-cdiff`
-
+print("Loading packages...")
 # %
 import pandas as pd
 import numpy as np
 from sklearn.impute import SimpleImputer, KNNImputer
+print("Done.")
 
 # %%
+print("reading data...")
 training_data = pd.read_csv("training_data_full.csv.gz")
+# populate with np.NaN for compatibility with skl
 training_data  = training_data.fillna(np.nan)
+print("Done.")
 
 # %%
-imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
+print("Setting simple imputer...")
+imputer = SimpleImputer(missing_values=np.nan, strategy="median")
 imputer.fit(X = training_data)
-
-#knn_imputer = KNNImputer(missing_values=np.nan, n_neighbors=5, weights="uniform")
-#knn_imputer.fit(X = training_data)
+print("Done.")
 
 # %%
-imputed_array = imputer.transform(training_data)
-#imputed_array = knn_imputer.transform(training_data)
-training_data_imputed_df = pd.DataFrame(imputed_array, columns=training_data.columns, index=training_data.index)
-# training_data_imputed_df 
+print("Running simple impute...")
+imputed_array_simple = imputer.transform(training_data)
+training_data_imputed_simple_df = pd.DataFrame(imputed_array_simple, columns=training_data.columns, index=training_data.index)
+print("Done.")
+
+print("Writing simple CSV...")
+training_data_imputed_simple_df.to_csv("training_data_imputed_simple_full.csv.gz", index=False)
+print("Done.")
+
+print("Setting kNN imputer...")
+# n_jobs to maximize parallelism
+knn_imputer = KNNImputer(missing_values=np.nan, n_neighbors=2, weights="uniform", n_jobs=-1)
+knn_imputer.fit(X = training_data)
+print("Done.")
+print("Running kNN impute...")
+imputed_array_knn = knn_imputer.transform(training_data)
+training_data_imputed_kNN_df = pd.DataFrame(imputed_array_kNN, columns=training_data.columns, index=training_data.index)
+print("Done.")
 
 # %%
-training_data_imputed_df.to_csv("training_data_imputed_full.csv.gz", index=False)
+print("Writing kNN CSV...")
+training_data_imputed_kNN_df.to_csv("training_data_imputed_kNN_full.csv.gz", index=False)
+print("Done.")
--- slurmsession_impute.sh
+++ slurmsession_impute.sh
@@ -1,1 +1,1 @@
-sinteractive --qos=debug --time=00:12:00 --nodes=2 --mem=16000MB  --account=bios26406
+sinteractive  --time=00:10:00 --nodes=16 --mem=32GB  --account=bios26406
