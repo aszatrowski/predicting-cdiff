a694289e38caa525f8d08a923573d4e12909f0c1
Austin Szatrowski <austinszatrowski@gmail.com>
2025-05-24 16:50:17 -0500

    survival works now, best model saved as .joblib

--- slurmsession_train_surv.sh
+++ slurmsession_train_surv.sh
@@ -1,1 +1,1 @@
-sinteractive  --time=01:00:00 --qos=debug --nodes=4 --ntasks-per-node=10 --mem-per-cpu=8GB  --account=bios26406
+sinteractive  --time=01:00:00 --nodes=4 --ntasks-per-node=15 --mem-per-cpu=8GB  --account=bios26406
